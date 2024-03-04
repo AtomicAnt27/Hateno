a1305ecda6e068eb61b0aeef06fe3806ac6e2dd8
AtomicAnt27 <pedrodafonsecaschwertner@gmail.com>
2024-03-04 20:38:57 -0300

    json caralhooo

--- Cardapio/cardapio_script.js
+++ Cardapio/cardapio_script.js
@@ -15,3 +15,31 @@ function refresh(){
         cardapio.classList.remove('hidden');
     });
 }
+
+const render = (card) => {
+    var card = $("card");
+    card.innerHTML = "";
+
+    var img = document.createElement("img");
+    img.className = "card-text";
+    card.appendChild(img);
+
+    var card_body = document.createElement("div");
+    card_body.className = "card-body";
+    card_body.innerHTML = "";
+    card.appendChild(card_body);
+
+    var title = document.createElement("h5");
+    title.className = "card-title";
+    title.textContent = prova.name;
+    card_body.appendChild(title);
+
+    var text = document.createElement("p");
+    text.className = "card-text";
+    card_body.appendChild(text);
+
+    var cart_button = document.createElement("a");
+    cart_button.className = "btn btn-primary";
+    cart_button.setAttribute('href', "#")
+    card_body.appendChild(cart_button);
+}
